8fe05a70fce721889d3320e2461dde8edf01a00e
elisaaivarone <lisa.sandrade@yahoo.com.br>
2019-09-10 18:02:17 -0300

    create react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import firebase from "./firebaseConfig";
+//import firebase from "./firebaseConfig";
 class App extends React.Component {
   constructor(props) {
     super(props);
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "<sua-chave>",
-  authDomain: "<sua-chave>",
-  databaseURL: "<sua-chave>",
-  projectId: "<sua-chave>",
-  storageBucket: "<sua-chave>",
-  messagingSenderId: "<sua-chave>",
-  appId: "<sua-chave>"
+  apiKey: "AIzaSyC2uBvfIaffS--pHzt9Pgf_gwBg8EqA4-8",
+    authDomain: "burger-queen-c709c.firebaseapp.com",
+    databaseURL: "https://burger-queen-c709c.firebaseio.com",
+    projectId: "burger-queen-c709c",
+    storageBucket: "",
+    messagingSenderId: "252062502278",
+    appId: "1:252062502278:web:c15e2d4c7b39ee8bb4b007"
 };
 
 firebase.initializeApp(config);
